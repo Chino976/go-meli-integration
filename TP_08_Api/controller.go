554d7348a567f9e0f5904614f1aabb5045075db8
Federico Bettic <federicobettic@gmail.com>
2020-06-19 13:08:01 -0300

    Fix controller.go

--- TP_08_Api/controller.go
+++ TP_08_Api/controller.go
@@ -10,13 +10,13 @@ func sum(c *gin.Context){
 	a := c.Query("a")
 	b := c.Query("b")
 	if(a == "" || b == ""){
-		c.String(400, "ERROR 400 - Variables vacias")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	A, err1:= strconv.ParseFloat(a, 64)
 	B, err2:= strconv.ParseFloat(b, 64)
 	if(err1 != nil || err2 !=nil){
-		c.String(400, "ERROR 400 - Conversion de variables fallida")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	c.String(200, "{\"result\": %v}", A+B)
@@ -27,13 +27,13 @@ func res(c *gin.Context){
 	b := c.Query("b")
 
 	if(a == "" || b == ""){
-		c.String(400, "ERROR 400 - Variables vacias")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	A, err1:= strconv.ParseFloat(a, 64)
 	B, err2:= strconv.ParseFloat(b, 64)
 	if(err1 != nil || err2 !=nil){
-		c.String(400, "ERROR 400 - Conversion de variables fallida")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	c.String(200, "{\"result\": %v}", A-B)
@@ -43,13 +43,13 @@ func mul(c *gin.Context){
 	a := c.Query("a")
 	b := c.Query("b")
 	if(a == "" || b == ""){
-		c.String(400, "ERROR 400 - Variables vacias")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	A, err1:= strconv.ParseFloat(a, 64)
 	B, err2:= strconv.ParseFloat(b, 64)
 	if(err1 != nil || err2 !=nil){
-		c.String(400, "ERROR 400 - Conversion de variables fallida")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	c.String(200, "{\"result\": %v}", A*B)
@@ -59,17 +59,17 @@ func div(c *gin.Context){
 	a := c.Query("a")
 	b := c.Query("b")
 	if(a == "" || b == ""){
-		c.String(400, "ERROR 400 - Variables vacias")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	A, err1:= strconv.ParseFloat(a, 64)
 	B, err2:= strconv.ParseFloat(b, 64)
 	if(err1 != nil || err2 !=nil){
-		c.String(400, "ERROR 400 - Conversion de variables fallida")
+		c.String(400, "HTTP 400 Missing params a,b")
 		return
 	}
 	if(B==0){
-		c.String(400, "ERROR 400 - Se esta intentando dividir por 0")
+		c.String(400, "HTTP 400 Invalid param b, cannot be 0 in div")
 		return
 	}
 	c.String(200, "{\"result\": %v}", A/B)
